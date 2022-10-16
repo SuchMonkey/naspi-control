7741778226472c2c62748cebc09aae0cb37940c1
David D <suchmonkey@protonmail.com>
2022-10-16 15:09:59 +0200

    add mod

---
+++ src/fan_control.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+
+use rppal::pwm::{Channel, Polarity, Pwm};
+use systemstat::{Platform, System};
+
+pub struct FanControl {
+    pwm: Pwm,
+    sys: System,
+    temp_range: (f32, f32),
+}
+
+impl FanControl {
+    pub fn setup() -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            pwm: Pwm::with_frequency(Channel::Pwm0, 25000.0, 1.0, Polarity::Normal, true)?,
+            sys: System::new(),
+            temp_range: (30.0, 75.0),
+        })
+    }
+
+    pub fn update(&self) -> Result<(), Box<dyn Error>> {
+        let mut duty_cycle = 1.0;
+
+        let (min, max) = self.temp_range;
+
+        if let Ok(temp) = self.sys.cpu_temp() {
+            let temp = temp.clamp(min, max);
+
+            duty_cycle = (temp - min) / (max - min);
+        }
+
+        self.pwm.set_duty_cycle(duty_cycle as f64)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-use rppal::pwm::{Channel, Polarity, Pwm};
 use simple_signal::{self, Signal};
-use systemstat::{Platform, System};
+
+use crate::fan_control::FanControl;
+
+mod fan_control;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let running = Arc::new(AtomicBool::new(true));
@@ -28,36 +30,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-struct FanControl {
-    pwm: Pwm,
-    sys: System,
-    temp_range: (f32, f32),
-}
-
-impl FanControl {
-    pub fn setup() -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            pwm: Pwm::with_frequency(Channel::Pwm0, 25000.0, 1.0, Polarity::Normal, true)?,
-            sys: System::new(),
-            temp_range: (30.0, 75.0),
-        })
-    }
-
-    pub fn update(&self) -> Result<(), Box<dyn Error>> {
-        let mut duty_cycle = 1.0;
-
-        let (min, max) = self.temp_range;
-
-        if let Ok(temp) = self.sys.cpu_temp() {
-            let temp = temp.clamp(min, max);
-
-            duty_cycle = (temp - min) / (max - min);
-        }
-
-        self.pwm.set_duty_cycle(duty_cycle as f64)?;
-
-        Ok(())
-    }
 }
